66e93c7348d366018134098ce1dfbdb692e842c9
Moniruliptv <moniruliptvserver@gmail.com>
2026-02-18 12:02:14 +0600

    Auto update AYNA token

--- ayna_token.php
+++ ayna_token.php
@@ -1,6 +1,3 @@
-<?php
-header('Content-Type: application/json; charset=UTF-8');
-echo <<<JSON
 {
     "code": 1,
     "message": "Success",
@@ -11,10 +8,9 @@ echo <<<JSON
         "token": {
             "expires_in": 43200,
             "token_type": "Bearer",
-            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNzhiZTY2NDQtMGE2NS00OGVjLTgxYTQtMDg5YWM2NWEyNjE5Iiwib3BlcmF0b3JJZCI6IjFmYjFiNGM3LWRiZDktNDY5ZS04OGEyLWMyMDdkYzE5NTg2OSIsImdlb0lwUnVsZXMiOnsiZDE3MjUwYTMtMzk4NS00OWJjLWFlOGMtZmU3Y2JiNjA0MDhjIjp0cnVlfSwiZ2VvTG9jYXRpb24iOnsiY291bnRyeSI6IkJEIiwiY2l0eSI6IkZhcsSrZHB1ciIsImlwIjoiMTYzLjIyNy4xNDQuMTc2In0sImRldmljZUlkIjoiNTgyQ0M3ODhEMjUwQ0ZBRENBMUI2OTREODY4Mjg4QTcifSwiaWF0IjoxNzcxMzkzOTE4LCJleHAiOjE3NzE0MzcxMTh9.ILw5fJnhDYxISSN4tybminJzt8_K1DXx-7hn6ExqB6A",
-            "refresh_token": "1HGL3q4oB1psMZH5iPsvXoix1jULeLZKqM0BNvapdRJsknfCFHY0bcoTqJepivJF84aBtL9EYroUsmZEscGuXHtpDaO1Hp8a3SurGvTL3Rg6bm3R1o0FWJVUytI4k9efkGv3tNPnkU6UEfLJbJ4nYu6JRriqpJUkKLCDKxZZ2vDHcneLv3AOOspL6Ha3GDo6NnNUsUoYLWRPt7d3nyWd58WMIomI9OqFakYL6LYnOE39nfIid0t6wo6IeGBIUVTo"
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNzhiZTY2NDQtMGE2NS00OGVjLTgxYTQtMDg5YWM2NWEyNjE5Iiwib3BlcmF0b3JJZCI6IjFmYjFiNGM3LWRiZDktNDY5ZS04OGEyLWMyMDdkYzE5NTg2OSIsImdlb0lwUnVsZXMiOnsiZDE3MjUwYTMtMzk4NS00OWJjLWFlOGMtZmU3Y2JiNjA0MDhjIjp0cnVlfSwiZ2VvTG9jYXRpb24iOnsiY291bnRyeSI6IkJEIiwiY2l0eSI6IkZhcsSrZHB1ciIsImlwIjoiMTYzLjIyNy4xNDQuMTc2In0sImRldmljZUlkIjoiNTgyQ0M3ODhEMjUwQ0ZBRENBMUI2OTREODY4Mjg4QTcifSwiaWF0IjoxNzcxMzk0NTMzLCJleHAiOjE3NzE0Mzc3MzN9.PwnQlbP1ncn5QebjAseSVys1NAviQWFronsCQS6LfrM",
+            "refresh_token": "RS8b7usQN53sJ8u4usvjSxvMRKiuEjMp6ciZXgpmClzHWw2O2bYXkAw8SnG6jApQTrSB4tHrXxSVmXRHSdzUZPLDmgq8mbPv3N2YQkkJrlH6mLDh6gpUj2WALGsd0lDRDupv5jTfiXFLasALqeuenMO84GtuqQtyVjAQFPDDi736SqO8RGiUAncWoYMb76iMr0MikLNvXhMcTd9iJospd9po1MuVmZzXVU1H0bIXwU6xDJgeQRNsUEo8P4hKMoBx"
         }
     },
-    "execTime": 259
+    "execTime": 390
 }
-JSON;
