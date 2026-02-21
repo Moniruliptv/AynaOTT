577649c784e0a7c8df73e069ed6fa224fd5b0758
Moniruliptv <moniruliptvserver@gmail.com>
2026-02-21 20:56:13 +0600

    Auto update AYNA token

--- ayna_token.php
+++ ayna_token.php
@@ -1,5 +1,16 @@
 {
-    "code": 102,
-    "message": "User not found",
-    "execTime": 13
+    "code": 1,
+    "message": "Success",
+    "content": {
+        "user_id": "78be6644-0a65-48ec-81a4-089ac65a2619",
+        "operatorId": "1fb1b4c7-dbd9-469e-88a2-c207dc195869",
+        "status": 1,
+        "token": {
+            "expires_in": 43200,
+            "token_type": "Bearer",
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNzhiZTY2NDQtMGE2NS00OGVjLTgxYTQtMDg5YWM2NWEyNjE5Iiwib3BlcmF0b3JJZCI6IjFmYjFiNGM3LWRiZDktNDY5ZS04OGEyLWMyMDdkYzE5NTg2OSIsImdlb0lwUnVsZXMiOnsiZDE3MjUwYTMtMzk4NS00OWJjLWFlOGMtZmU3Y2JiNjA0MDhjIjp0cnVlfSwiZ2VvTG9jYXRpb24iOnsiY291bnRyeSI6IkJEIiwiY2l0eSI6IkZhcsSrZHB1ciIsImlwIjoiMTYzLjIyNy4xNDQuMTc4In0sImRldmljZUlkIjoiNTgyQ0M3ODhEMjUwQ0ZBRENBMUI2OTREODY4Mjg4QTcifSwiaWF0IjoxNzcxNjg1NzcyLCJleHAiOjE3NzE3Mjg5NzJ9.3R0PRxZ7bjrS0R9-bu9sC5NVEmwzQChqLn3IaM_1U4k",
+            "refresh_token": "knr5hCVJ1cOsCWRQ1x5U9a1oMco6wIBKCkvuDgWpzn9lfGH0BqBJa85mYM77ZmfwdC37jWQLGN1fJqvWlLBeeA4ONWLvoXr8VbjAk6ep3p1RTZ8MSMaTvTBO5XbJqQmYNSb3XvE0TzqZjEBLsyn9I7w2GGElxnJLZ5GUoTXi49PaErVvoaaWbMEIBMN2VNzUIj9cyBEHmNRrRQc4HOialMHERDFIWqHwnFJeXRubBylaczGbKBOfJvVktlX4ydi6"
+        }
+    },
+    "execTime": 244
 }
